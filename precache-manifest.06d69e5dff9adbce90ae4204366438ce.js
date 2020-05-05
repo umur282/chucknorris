2e55146fbf8333d17b9d3f4f49a77b8d25e3cb96
umur282 <umur.282@gmail.com>
2020-05-05 10:58:01 +0200

    Updates

---
+++ precache-manifest.06d69e5dff9adbce90ae4204366438ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8b5631076bdfb2ab79e67c0c8c01437",
+    "url": "/chucknorris/index.html"
+  },
+  {
+    "revision": "cfdfe09d76508a02b638",
+    "url": "/chucknorris/static/css/2.7dcdf952.chunk.css"
+  },
+  {
+    "revision": "d23b526124fec5c3eeb1",
+    "url": "/chucknorris/static/css/main.2067da0c.chunk.css"
+  },
+  {
+    "revision": "cfdfe09d76508a02b638",
+    "url": "/chucknorris/static/js/2.fe453e92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chucknorris/static/js/2.fe453e92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d23b526124fec5c3eeb1",
+    "url": "/chucknorris/static/js/main.e3d46621.chunk.js"
+  },
+  {
+    "revision": "c570429f463f277e3938",
+    "url": "/chucknorris/static/js/runtime-main.aa10f85e.js"
+  }
+]);
